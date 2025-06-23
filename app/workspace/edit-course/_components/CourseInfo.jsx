b37cc976780ea0e58bb6d25929d3902f94dbb62d
Mighty-Sarthak-07 <sarthak230504@gmail.com>
2025-06-23 22:31:00 +0530

    add courseinfo

--- app/workspace/edit-course/_components/CourseInfo.jsx
+++ app/workspace/edit-course/_components/CourseInfo.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import { BookOpen, Clock, TrendingUp } from 'lucide-react';
+import Image from 'next/image';
 
 function CourseInfo({course}) {
     const courseLayout = course?.courseJson?.course;
   return (
     <div>
-        <div>
+        <div className='flex flex-col gap-4'>
             <h2 className='text-2xl font-bold'>{courseLayout?.name}</h2>
-            <p className='text-gray-500'>{courseLayout?.description}</p>
+            <p className='text-xl text-gray-700'>{courseLayout?.description}</p>
+            <div className='gap-2 grid grid-cols-1 md:grid-cols-3'>
+                <div className='flex items-center gap-6 p-3 rounded-xl mt-2 bg-purple-100 shadow-sm'>
+                    <Clock className='w-10 h-10 text-purple-500'/>
+                    <section>
+                        <h1 className="font-bold">Course Duration</h1>
+                        <h2>4 hours</h2>
+                    </section>
+                </div>
+                <div className='flex items-center gap-6 p-3 rounded-xl mt-2 bg-blue-100 shadow-sm'>
+                    <BookOpen className='w-10 h-10 text-blue-500'/>
+                    <section>
+                        <h1 className="font-bold">Chapters</h1>
+                        <h2>{courseLayout?.chapters?.length}</h2>
+                    </section>
+                </div>
+                <div className='flex items-center gap-6 p-3 rounded-xl mt-2 bg-red-100 shadow-sm'>
+                    <TrendingUp className='w-10 h-10 text-red-500'/>
+                    <section>
+                        <h1 className="font-bold">Difficulty Level</h1>
+                        <h2>{courseLayout?.level}</h2>
+                    </section>
+                </div>
+            </div>
+            <Image src={courseLayout?.bannerImageUrl} alt="banner image" width={400} height={400} className='rounded-xl w-full h-[240px] object-cover'/>
         </div>
     </div>
   )
