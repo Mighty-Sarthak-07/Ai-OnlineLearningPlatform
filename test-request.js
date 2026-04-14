236ca2b87cb52715353a8b10a6395e763032481e
Mighty-Sarthak-07 <sarthak230504@gmail.com>
2026-04-15 01:23:46 +0530

    done

--- test-request.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:3001/api/ai-tasks', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    courseTitle: "Test Course",
-    chapters: [{name: "Chapter 1"}],
-    deadline: "2026-05-01"
-  })
-}).then(res => res.json()).then(console.log).catch(console.error);
